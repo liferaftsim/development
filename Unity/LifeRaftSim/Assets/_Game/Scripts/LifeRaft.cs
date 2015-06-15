ccfa1b798e710b2b044117e11fa5c0110284bdd2
Robin Theilade <robin.theilade@gmail.com>
2015-06-15 20:13:40 +0200

    Change life raft scripted rotation to physics

--- Unity/LifeRaftSim/Assets/_Game/Scripts/LifeRaft.cs
+++ Unity/LifeRaftSim/Assets/_Game/Scripts/LifeRaft.cs
@@ -9,18 +9,6 @@ namespace Game
     public class LifeRaft : MonoBehaviour
     {
         /// <summary>
-        /// The minimum amount of random rotation.
-        /// A negative amount will make it rotate the other way around.
-        /// </summary>
-        public float minimumRandomRotation = -1.0f;
-
-        /// <summary>
-        /// The maximum amount of random rotation.
-        /// A negative amount will make it rotate the other way around.
-        /// </summary>
-        public float maximumRandomRotation = 1.0f;
-
-        /// <summary>
         /// The direction and speed of the raft.
         /// </summary>
         public Vector3 velocity;
@@ -41,14 +29,6 @@ namespace Game
         /// <summary>
         /// Called by Unity.
         /// </summary>
-        private void Update()
-        {
-            this.Rotate();
-        }
-
-        /// <summary>
-        /// Called by Unity.
-        /// </summary>
         private void FixedUpdate()
         {
             this.Move();
@@ -63,20 +43,11 @@ namespace Game
         }
 
         /// <summary>
-        /// Rotates the raft by a random amount each update call.
-        /// </summary>
-        private void Rotate()
-        {
-            var randomRotation = Random.Range(this.minimumRandomRotation, this.maximumRandomRotation);
-            this.transform.Rotate(0.0f, randomRotation * Time.deltaTime, 0.0f);
-        }
-
-        /// <summary>
         /// Moves the raft by a constant amount each update call.
         /// </summary>
         private void Move()
         {
-            this.rigidbody.AddForce(this.velocity, ForceMode.Impulse);
+            this.rigidbody.AddForceAtPosition(this.velocity, this.transform.position - this.velocity, ForceMode.Impulse);
         }
     }
 }
